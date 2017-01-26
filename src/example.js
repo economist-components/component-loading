4f59636f0ecbcb58cae21b79f8b91b5e0f63b16f
mortenko <zeleny.maros@netkosice.sk>
2017-01-26 12:58:51 +0100

    feat: add missed tests and use props loadingText instead of props loadContent
    it was added three unit tests and used props loadingText instead of loadingContent

--- src/example.js
+++ src/example.js
@@ -1,25 +1,17 @@
 import Loading from './index';
 import React from 'react';
 
-
 export default (
   <div>
     <p>Default with no overlay</p>
-    <button>
-      <a href="#" >
-      <Loading loadContent="the browser is loading new context" />
-     </a>
-    </button>
-    <Loading loadContent="the browser is loading new context" />
+    <Loading loadingText="the browser is loading new context" />
     <br />
     <div className="overlay">
       Just a test that is overlayed by the loader
-      <Loading loadingText="We are loading something..." className="loading--overlay"
-        loadContent="the browser is loading new context"
-      />
+      <Loading loadingText="The browser is loading new content" className="loading--overlay" />
     </div>
     <p>With 2 seconds of delay</p>
-    <Loading delay={2000} loadContent="the browser is loading new context" />
+    <Loading delay={2000} loadingText="the browser is loading new context" />
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ export default class Loading extends React.Component {
     return {
       className: React.PropTypes.string,
       overlay: React.PropTypes.bool,
-      loadingText: React.PropTypes.string,
+      loadingText: React.PropTypes.string.isRequired,
       delay: React.PropTypes.number,
-      loadContent: React.PropTypes.string.isRequired,
     };
   }
 
@@ -20,21 +19,16 @@ export default class Loading extends React.Component {
   render() {
     const extraClassNames = this.props.className ? this.props.className.split(/\s+/g) : [];
     const els = [];
-    if (this.props.loadContent) {
-      els.push(<span className="loading__content" key="loading__content" role="status">{this.props.loadContent}
-      </span>);
-    }
     if (this.props.overlay) {
       els.push(<div className="loading__overlay" key="loading__overlay"></div>);
     }
-
     els.push(<div className="loading__inner" key="loading__inner"></div>);
     if (this.props.loadingText) {
       els.push(
         <div className="loading__text" key="loading__text">
-          <div className="loading__text-inner">
+          <span role="status" className="loading__text-inner">
             {this.props.loadingText}
-          </div>
+          </span>
         </div>
       );
     }
